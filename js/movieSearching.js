9ca3f59120f7ee7ada93ca2d75c6fcb29dfd2280
KEastWiseman <2ehgus6260@gmail.com>
2024-04-29 16:51:14 +0900

    implement card click action

--- js/movieSearching.js
+++ js/movieSearching.js
@@ -1,4 +1,4 @@
-function makeMovieCard(title, overView, image, star) {
+function makeMovieCard(title, overView, image, star, id) {
     let newCardDiv = document.createElement("div");
     let newCardBackground = document.createElement('div');
     let newCardTitle = document.createElement("div");
@@ -11,6 +11,7 @@ function makeMovieCard(title, overView, image, star) {
 
 
     newCardDiv.setAttribute("class","card");
+    newCardDiv.setAttribute("onclick",`alert('movie id : ${id}')`)
     newCardBackground.setAttribute("class","cardBackground")
     newCardBackground.setAttribute("style",`
         background-image: url("https://image.tmdb.org/t/p/w500${image}") 
@@ -27,4 +28,26 @@ function makeMovieCard(title, overView, image, star) {
     newCardDiv.appendChild(newCardBackground);
 
     document.querySelector(".container > .center").appendChild(newCardDiv)
+}
+
+
+function callMovieData(title){
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhOTJlZDFjNTJkOTRjZjBiZDAxZWM5MDdlYjVkZjVlMSIsInN1YiI6IjY2MmI0OGNkMDFiMWNhMDExY2UzZjE4OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kfsG0XrqjvjEinnMIHAGVtd7WbxUFpXPKj_CF_M-k08'
+        }
+    };
+    fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
+        .then((response) => response.json())
+        .then((json)=>{
+            data=json.results;
+
+            data.forEach(movie => {
+                console.log(`${movie['title']} ?? ${title}`)
+                makeMovieCard(`${movie['title']}`,`${movie['overview']}`,`${movie['poster_path']}`,`${movie['vote_average']}`,`${movie['id']}`);
+            });    
+        })
+        .catch(err => console.error(err));
 }
